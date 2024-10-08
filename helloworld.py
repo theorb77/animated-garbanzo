70fd2d3855c6eb2028d698a4f8a508b713f5a0ea
Jared Newbold <theorb77@gmail.com>
2024-10-07 20:36:19 -0700

    commenting out dynamo calls

--- helloworld.py
+++ helloworld.py
@@ -6,23 +6,24 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
-dynamodb = boto3.resource('dynamodb', 
-           region_name=os.environ['AWS_REGION'], 
-           aws_access_key_id=os.environ['ACCESS_ID'], 
-           aws_secret_access_key=os.environ['ACCESS_KEY'])
-table = dynamodb.Table("simpletable")
-result = table.scan()
-if result is not None:
-    item = result['Items'][0]['simplekey']
-else:
-    raise Exception("Couldn't retrieve 'Hello World'")
+#dynamodb = boto3.resource('dynamodb', 
+#           region_name=os.environ['AWS_REGION'], 
+#           aws_access_key_id=os.environ['ACCESS_ID'], 
+#           aws_secret_access_key=os.environ['ACCESS_KEY'])
+#table = dynamodb.Table("simpletable")
+#result = table.scan()
+#if result is not None:
+    #item = result['Items'][0]['simplekey']
+#else:
+#    raise Exception("Couldn't retrieve 'Hello World'")
 
 @app.route('/')
 def hello_world():
-    if item is not None:
-        response = item
-    else:
-        response = '500 (Internal Server Error)'
+    response = "Hello World"
+ #   if item is not None:
+ #       response = item
+ #   else:
+ #       response = '500 (Internal Server Error)'
     return response
 
 @app.route('/health')
