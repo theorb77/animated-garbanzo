9cf7b6dcefc47eed7162fe729b9b3538b5b14136
Jared Newbold <theorb77@gmail.com>
2024-10-07 21:25:22 -0700

    removing token access, going to update IAM role

--- helloworld.py
+++ helloworld.py
@@ -7,9 +7,7 @@ from opentelemetry.instrumentation.flask import FlaskInstrumentor
 app = Flask(__name__)
 FlaskInstrumentor().instrument_app(app)
 dynamodb = boto3.resource('dynamodb', 
-           region_name=os.environ['AWS_REGION'], 
-           aws_access_key_id=os.environ['ACCESS_ID'], 
-           aws_secret_access_key=os.environ['ACCESS_KEY'])
+           region_name=os.environ['AWS_REGION'])
 table = dynamodb.Table("simpletable")
 result = table.scan()
 if result is not None:
